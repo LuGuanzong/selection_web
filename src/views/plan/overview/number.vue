f52ad53cbeaf1bbbfc74ec4196d15c5775e69042
luguanzong <746722756@qq.com>
2023-09-01 23:21:19 +0800

    完成数字概览样式

--- src/views/plan/overview/number.vue
+++ src/views/plan/overview/number.vue
@@ -7,23 +7,27 @@ defineOptions({ name: 'planOverview' })
 <template>
   <div class="number-content">
     <div class="data">
-      <div>
-        圆圈
+      <div class="circle-bg">
+        <img alt="Vue logo" class="logo" src="@/assets/histogram.svg" width="20" height="20" />
       </div>
-      <div>
-        数量
-      </div>
-      <div>
-        备注
-      </div>
+      <div class="count">21项</div>
+      <div class="desc">总计划</div>
     </div>
 
     <div class="data">
-      1
+      <div class="circle-bg">
+        <img alt="Vue logo" class="logo" src="@/assets/histogram.svg" width="20" height="20" />
+      </div>
+      <div class="count">11项</div>
+      <div class="desc">已完成计划</div>
     </div>
 
     <div class="data">
-      1
+      <div class="circle-bg">
+        <img alt="Vue logo" class="logo" src="@/assets/histogram.svg" width="20" height="20" />
+      </div>
+      <div class="count">10项</div>
+      <div class="desc">未完成计划</div>
     </div>
   </div>
 </template>
@@ -39,16 +43,35 @@ defineOptions({ name: 'planOverview' })
   justify-content: space-around;
   gap: 15px;
 
+  .count {
+    color: #f2f2f2;
+    font-size: 18px;
+  }
+
+  .desc {
+    color: #f2f2f2;
+    font-size: 12px;
+  }
 }
 
 .data {
   height: 100%;
   background: #6699CC;
-  flex: 1 0 auto;
+  flex: 1 0 30%;
   border-radius: 12px;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+  align-items: center;
+}
+
+.circle-bg {
+  background: #f2f2f2;
+  width:43px;
+  height: 43px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
   align-items: center;
 }
 </style>
