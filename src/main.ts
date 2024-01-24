be925cd0a3658b23e6f0a56e338cce808b547739
luguanzong <luguanzong@zego.im>
2024-01-25 01:36:42 +0800

    兄弟传值

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import 'virtual:svg-icons-register'
+import VueMitter from '@nguyenshort/vue3-mitt'
 
 const app = createApp(App)
+
+app.use(VueMitter) // 兄弟组件传值
 
 app.use(createPinia())
 app.use(router)
